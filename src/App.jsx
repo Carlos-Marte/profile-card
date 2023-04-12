e28ddeb6b8a47391bb0c5aab969590f7cacadea7
Carlos-Marte <carlosmartedev@hotmail.com>
2023-04-11 20:35:49 -0400

    Add styles to App and ProfileCard

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import VictorPhoto from "./assets/image-victor.jpg";
 
 const App = () => {
     return (
-        <ProfileCard
-            srcImg={VictorPhoto}
-            name="Victor Crest"
-            age={26}
-            location="London"
-            socialStats={{
-                followers: "80",
-                likes: "803",
-                photos: "1.4",
-            }}
-        />
+        <main className="bg-dark-cyan min-h-screen grid place-content-center place-items-center grid-cols-1 grid-flow-row lg:grid-flow-col gap-4 p-6 mx-auto overflow-hidden">
+            <ProfileCard
+                srcImg={VictorPhoto}
+                name="Victor Crest"
+                age={26}
+                location="London"
+                socialStats={{
+                    followers: "80",
+                    likes: "803",
+                    photos: "1.4",
+                }}
+            />
+        </main>
     );
 };
 
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,22 +1,28 @@
 import { SocialStats } from "./SocialStats";
+import bgImage from "../assets/bg-pattern-card.svg";
 
 export const ProfileCard = ({ srcImg, name, age, location, socialStats }) => {
     return (
-        <section>
-            <div>IMG BG</div>
+        <section className="w-full max-w-[350px] min-w-[250px] bg-white rounded-xl flex flex-col gap-4 items-center text-center overflow-hidden">
             <div>
-                <img src={srcImg} alt={`Photo of ${name}`} />
-
-                <div>
-                    <div>
-                        <h1>{name}</h1>
-                        <span>{age}</span>
+                <img src={bgImage} alt="Background pattern" loading="lazy" />
+            </div>
+            <div className="relative">
+                <img
+                    className="border-4 border-white rounded-full absolute -top-3/4 left-1/2 -translate-x-1/2 z-10"
+                    src={srcImg}
+                    alt={`Photo of ${name}`}
+                />
+                <div className="flex flex-col justify-end gap-1 pt-9">
+                    <div className="flex gap-2">
+                        <h1 className="font-bold">{name}</h1>
+                        <span className="text-dark-gray">{age}</span>
                     </div>
-                    <p>{location}</p>
+                    <p className="text-sm text-dark-gray">{location}</p>
                 </div>
             </div>
 
-            <div>SEPARATOR</div>
+            <div className="w-full h-px bg-dark-grayish-blue opacity-30"></div>
 
             <SocialStats socialStats={socialStats} />
         </section>
